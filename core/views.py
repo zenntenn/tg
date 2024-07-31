09dc808d7757df447c6d243778a6ff65c1eec92e
Charles Siegel <charles.m.siegel@gmail.com>
2024-07-31 06:38:00 -0700

    Adding HomeView

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class HomeView(View):
+    """This View controls the main landing page for the site"""
+
+    def get(self, request):
+        return render(request, "core/index.html", context={})
