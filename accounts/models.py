499d9b1f14a34dce14990be960bc710fa6dc779c
Charles Siegel <charles.m.siegel@gmail.com>
2024-08-01 04:39:34 -0700

    profile class Fixes #4

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    head_st = models.BooleanField(default=False)
+    vtm_st = models.BooleanField(default=False)
+    wta_st = models.BooleanField(default=False)
+    mta_st = models.BooleanField(default=False)
+    ctd_st = models.BooleanField(default=False)
+    wto_st = models.BooleanField(default=False)
+    htr_st = models.BooleanField(default=False)
+    dtf_st = models.BooleanField(default=False)
+    mtr_st = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from accounts import views
+
 # Create your URLs here
 urlpatterns = [
+    path("", views.ProfileView.as_view(), name="user"),
 ]
