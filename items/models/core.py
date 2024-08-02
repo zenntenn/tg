44d580007bad49171ac65560d71bfccefb740ee8
Charles Siegel <charles.m.siegel@gmail.com>
2024-08-02 04:54:44 -0700

    added location ownership functionality to items

--- items/models/core.py
+++ items/models/core.py
@@ -1,12 +1,16 @@
 from django.db import models
 from core.models import Model
 from characters.models.core import CharacterModel
+from locations.models.core import LocationModel
 
 # Create your models here.
 
 class ItemModel(Model):
     owned_by = models.ForeignKey(
         CharacterModel, blank=True, null=True, on_delete=models.SET_NULL
+    )
+    located_at = models.ForeignKey(
+        LocationModel, blank=True, null=True, on_delete=models.SET_NULL
     )
 
     class Meta:
