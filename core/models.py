7e1a314f2c781140137db1fd4e5df3acbcd5ad1e
Charles Siegel <charles.m.siegel@gmail.com>
2024-08-02 04:47:29 -0700

    fixed model locations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from polymorphic.models import PolymorphicModel
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Model(PolymorphicModel):
+    type = "model"
+
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+
+    status_keys = ["Un", "Sub", "App", "Ret", "Dec", "Ran", "Fre"]
+    statuses = [
+        "Unfinished",
+        "Submitted",
+        "Approved",
+        "Retired",
+        "Deceased",
+        "Random",
+        "Freeform",
+    ]
+    status = models.CharField(
+        max_length=3, choices=zip(status_keys, statuses), default="Un"
+    )
+    display = models.BooleanField(default=True)
+    description = models.TextField(default="")
+
+    class Meta:
+        abstract = True
+        verbose_name = "Model"
+        verbose_name_plural = "Models"
+
+    def __str__(self):
+        return self.name
+
+    def has_name(self):
+        return self.name != ""
+
+    def set_name(self, name):
+        self.name = name
+        return True
+
+    def has_description(self):
+        return self.description != ""
+
+    def set_description(self, description):
+        self.description = description
+        return True
+
+    def has_owner(self):
+        return self.owner is not None
+
+    def set_owner(self, owner):
+        self.owner = owner
+        return True
+
+    def update_status(self, status):
+        self.status = status
+        return True
+
+    def toggle_display(self):
+        self.display = not self.display
+        return True
+
