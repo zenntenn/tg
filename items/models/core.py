afe5768a171973605ded65b1de1ef791b19bc306
Charles Siegel <charles.m.siegel@gmail.com>
2024-08-02 18:39:27 -0700

    weapon base class Fixes #48

--- items/models/core.py
+++ items/models/core.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.urls import reverse
-from core.models import Model
+
 from characters.models.core import CharacterModel
+from core.models import Model
 from locations.models.core import LocationModel
 
 # Create your models here.
 
+
 class ItemModel(Model):
     type = "item"
-    
+
     owned_by = models.ForeignKey(
         CharacterModel, blank=True, null=True, on_delete=models.SET_NULL
     )
@@ -19,9 +21,38 @@ class ItemModel(Model):
     class Meta:
         verbose_name = "Item"
         verbose_name_plural = "Items"
-        
+
     def get_absolute_url(self):
         return reverse("items:item", args=[str(self.id)])
-    
+
     def get_heading(self):
         return "wod_heading"
+
+
+class Weapon(ItemModel):
+    type = "weapon"
+
+    class Meta:
+        verbose_name = "Weapon"
+        verbose_name_plural = "Weapons"
+
+    difficulty = models.IntegerField(default=0)
+    damage = models.IntegerField(default=0)
+    damage_type = models.CharField(
+        max_length=1,
+        default="L",
+        choices=[("B", "Bashing"), ("L", "Lethal"), ("A", "Aggravated")],
+    )
+    conceal = models.CharField(
+        max_length=1,
+        default="P",
+        choices=[
+            ("P", "Pocket"),
+            ("J", "Jacket"),
+            ("T", "Trenchcoat"),
+            ("N", "Not Applicable"),
+        ],
+    )
+
+    def get_update_url(self):
+        return reverse("wod:items:human:update_weapon", args=[str(self.id)])
--- items/tests.py
+++ items/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from items.models.core import ItemModel
+
+from items.models.core import ItemModel, Weapon
+
 
 # Create your tests here.
 class TestItemDetailView(TestCase):
@@ -13,3 +15,16 @@ class TestItemDetailView(TestCase):
     def test_object_detail_view_templates(self):
         response = self.client.get(f"/items/{self.item.id}/")
         self.assertTemplateUsed(response, "items/detail.html")
+
+
+class TestWeaponDetailView(TestCase):
+    def setUp(self) -> None:
+        self.item = Weapon.objects.create(name="Test Weapon")
+
+    def test_object_detail_view_status_code(self):
+        response = self.client.get(f"/items/{self.item.id}/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_object_detail_view_templates(self):
+        response = self.client.get(f"/items/{self.item.id}/")
+        self.assertTemplateUsed(response, "items/weapon/detail.html")
