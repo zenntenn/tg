6d04f4e018ec77b6f3bd566b87ce71eb5f0c7fe3
Charles Siegel <charles.m.siegel@gmail.com>
2024-08-03 04:25:51 -0700

    medium class Fixes #16

--- items/models/core.py
+++ items/models/core.py
@@ -83,3 +83,23 @@ class RangedWeapon(Weapon):
     class Meta:
         verbose_name = "Ranged Weapon"
         verbose_name_plural = "Ranged Weapons"
+
+
+class Medium(models.Model):
+    """Class managing Medium data"""
+
+    name = models.TextField(default="")
+    length_modifier_type = models.CharField(
+        max_length=1, default="/", blank=True, null=True
+    )
+    length_modifier = models.IntegerField(default=1, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Medium"
+        verbose_name_plural = "Media"
+
+    def get_absolute_url(self):
+        return reverse("medium", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return f"{self.name}"
