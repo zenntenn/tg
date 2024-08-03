94a84d78125d934800371fe4331dd3a513fcbb2b
Charles Siegel <charles.m.siegel@gmail.com>
2024-08-03 03:54:08 -0700

    added meleeweapon to admin

--- items/admin.py
+++ items/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from items.models.core import ItemModel, Weapon
+from items.models.core import ItemModel, MeleeWeapon, Weapon
 
 
 # Register your models here.
@@ -9,3 +9,4 @@ class ItemAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Weapon)
+admin.site.register(MeleeWeapon)
