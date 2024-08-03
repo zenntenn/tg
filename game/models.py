6aabcbcf126eec9dd052a214b1de7f8e0477d9f4
Charles Siegel <charles.m.siegel@gmail.com>
2024-08-03 06:15:44 -0700

    story class Fixes #24

--- game/models.py
+++ game/models.py
@@ -37,3 +37,47 @@ class Chronicle(models.Model):
 
     def storyteller_list(self):
         return ", ".join([x.username for x in self.storytellers.all()])
+
+    def total_stories(self):
+        return Story.objects.filter(chronicle=self).count()
+
+    def add_story(self, name):
+        if Story.objects.filter(name=name, chronicle=self).exists():
+            return Story.objects.filter(name=name, chronicle=self).first()
+        return Story.objects.create(name=name, chronicle=self)
+
+
+class Story(models.Model):
+    name = models.CharField(max_length=100, default="")
+    pcs = models.ManyToManyField(
+        "characters.CharacterModel", blank=True, related_name="pc_in"
+    )
+    key_npcs = models.ManyToManyField(
+        "characters.CharacterModel", blank=True, related_name="npc_in"
+    )
+    plot_summary = models.TextField(default="")
+    key_locations = models.ManyToManyField("locations.LocationModel", blank=True)
+    chronicle = models.ForeignKey(
+        "game.Chronicle", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Story"
+        verbose_name_plural = "Stories"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("game:story", kwargs={"pk": self.pk})
+
+    def total_locations(self):
+        return self.key_locations.count()
+
+    def total_pcs(self):
+        return self.pcs.count()
+
+    def total_npcs(self):
+        return self.key_npcs.count()
--- game/views.py
+++ game/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.views import View
-from game.models import Chronicle
+from game.models import Chronicle, Story
+from locations.models.core import LocationModel
 
 
 # Create your views here.
@@ -9,6 +10,7 @@ class ChronicleDetailView(View):
         chronicle = Chronicle.objects.get(pk=pk)
         return {
             "object": chronicle,
+            "stories": Story.objects.filter(chronicle=chronicle).order_by("start_date"),
         }
 
     def get(self, request, *args, **kwargs):
@@ -18,3 +20,24 @@ class ChronicleDetailView(View):
     def post(self, request, *args, **kwargs):
         context = self.get_context(kwargs["pk"])
         return redirect(context["object"].add_story(request.POST["name"]))
+
+
+class StoryDetailView(View):
+    def get_context(self, pk):
+        story = Story.objects.get(pk=pk)
+        return {
+            "object": story,
+        }
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context(kwargs["pk"])
+        return render(request, "game/story/detail.html", context)
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context(kwargs["pk"])
+        loc = LocationModel.objects.get(pk=request.POST["location"])
+        return redirect(
+            context["object"].add_scene(
+                request.POST["name"], loc, date_of_scene=request.POST["date_of_scene"]
+            )
+        )
