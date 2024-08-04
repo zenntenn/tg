f89c432d8dced4354c6533e32b23cc14f0a1f25a
Charles Siegel <charles.m.siegel@gmail.com>
2024-08-04 04:59:05 -0700

    city location subclass Fixes #80

--- locations/admin.py
+++ locations/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from locations.models.core import LocationModel
+from locations.models.core import City, LocationModel
 
 # Register your models here.
 admin.site.register(LocationModel)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent")
--- locations/models/core.py
+++ locations/models/core.py
@@ -1,4 +1,4 @@
-from characters.models.core import CharacterModel
+from characters.models.core import Character, CharacterModel
 from core.models import Model
 from django.db import models
 from django.urls import reverse
@@ -29,7 +29,29 @@ class LocationModel(Model):
         verbose_name_plural = "Location"
 
     def get_absolute_url(self):
-        return reverse("wod:locations:location", args=[str(self.id)])
+        return reverse("locations:location", args=[str(self.id)])
+
+    def get_heading(self):
+        return "wod_heading"
+
+
+class City(LocationModel):
+    type = "city"
+
+    population = models.IntegerField(default=0)
+    characters = models.ManyToManyField(Character, blank=True)
+    mood = models.TextField(blank=True, null=True)
+    theme = models.TextField(blank=True, null=True)
+    media = models.TextField(blank=True, null=True)
+    politicians = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "City"
+        verbose_name_plural = "Cities"
+
+    def add_character(self, character):
+        self.characters.add(character)
+        self.save()
 
     def get_heading(self):
         return "wod_heading"
