63135306525720c508fd104d67efe833dc1f4c94
Charles Siegel <charles.m.siegel@gmail.com>
2024-08-14 19:09:53 -0700

    Characters Indexes Fixes #181

--- characters/views/core/__init__.py
+++ characters/views/core/__init__.py
@@ -1,9 +1,11 @@
 from characters.models.core import Character, Derangement, Group, Human
 from characters.views import werewolf
+from core.utils import get_gameline_name
 from django.forms import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic import CreateView, DetailView, UpdateView, View
+from game.models import Chronicle, ObjectType
 
 from .archetype import ArchetypeCreateView, ArchetypeDetailView, ArchetypeUpdateView
 from .character import CharacterCreateView, CharacterDetailView, CharacterUpdateView
@@ -46,3 +48,41 @@ class GenericGroupDetailView(View):
         group = Group.objects.get(pk=kwargs["pk"])
         if group.type in self.group_views:
             return self.group_views[group.type].as_view()(request, *args, **kwargs)
+
+
+class CharacterIndexView(View):
+    def get(self, request, *args, **kwargs):
+        context = self.get_context(kwargs)
+        return render(request, "characters/index.html", context)
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context(kwargs)
+        return render(request, "characters/index.html", context)
+
+    def get_context(self, kwargs):
+        gameline = kwargs["gameline"]
+        game_characters = ObjectType.objects.filter(gameline=gameline, type="char")
+        game_character_types = [x.name for x in game_characters]
+        context = {"objects": game_characters, "gameline": get_gameline_name(gameline)}
+
+        chron_char_dict = {}
+        chron_group_dict = {}
+        for chron in list(Chronicle.objects.all()) + [None]:
+            c = Character.objects.filter(chronicle=chron).order_by("name")
+            g = Group.objects.filter(chronicle=chron).order_by("name")
+            characters = [x for x in c if x.type in game_character_types]
+            groups = [x for x in g if x.type in game_character_types]
+
+            c = Character.objects.filter(
+                id__in=[x.id for x in characters], chronicle=chron
+            ).order_by("name")
+            g = Group.objects.filter(
+                id__in=[x.id for x in groups], chronicle=chron
+            ).order_by("name")
+            chron_char_dict[chron] = c
+            chron_group_dict[chron] = g
+
+        context["chron_char_dict"] = chron_char_dict
+        context["chron_group_dict"] = chron_group_dict
+
+        return context
