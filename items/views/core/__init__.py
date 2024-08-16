7e42a8c9bf0000e49ce3c45c8cd8747f7350139f
Charles Siegel <charles.m.siegel@gmail.com>
2024-08-16 05:04:04 -0700

    item creation form Fixes #236

--- items/views/core/__init__.py
+++ items/views/core/__init__.py
@@ -1,8 +1,24 @@
+from collections import defaultdict
+
 from core.utils import get_gameline_name
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
 from django.views import View
 from game.models import Chronicle, ObjectType
+from items.forms.core.item_creation import ItemCreationForm
 from items.models.core.item import ItemModel
+from items.models.core.material import Material
+from items.models.core.medium import Medium
+from items.models.core.meleeweapon import MeleeWeapon
+from items.models.core.rangedweapon import RangedWeapon
+from items.models.core.thrownweapon import ThrownWeapon
+from items.models.core.weapon import Weapon
+from items.models.mage.artifact import Artifact
+from items.models.mage.charm import Charm
+from items.models.mage.grimoire import Grimoire
+from items.models.mage.talisman import Talisman
+from items.models.mage.wonder import Wonder
+from items.models.werewolf.fetish import Fetish
 from items.views import mage, werewolf
 
 from .item import ItemCreateView, ItemDetailView, ItemUpdateView
@@ -45,15 +61,61 @@ class GenericItemDetailView(View):
         item = ItemModel.objects.get(pk=kwargs["pk"])
         if item.type in self.views:
             return self.views[item.type].as_view()(request, *args, **kwargs)
+        return redirect("items:index")
 
 
 class ItemIndexView(View):
+    items = {
+        "item": ItemModel,
+        "weapon": Weapon,
+        "melee_weapon": MeleeWeapon,
+        "thrown_weapon": ThrownWeapon,
+        "ranged_weapon": RangedWeapon,
+        "wonder": Wonder,
+        "charm": Charm,
+        "artifact": Artifact,
+        "talisman": Talisman,
+        "grimoire": Grimoire,
+        "fetish": Fetish,
+        "material": Material,
+        "medium": Medium,
+    }
+
     def get(self, request, *args, **kwargs):
         context = self.get_context(kwargs)
         return render(request, "items/index.html", context)
 
     def post(self, request, *args, **kwargs):
         context = self.get_context(kwargs)
+        action = request.POST.get("action")
+        item_type = request.POST["item_type"]
+        gameline = kwargs["gameline"]
+        if action == "create":
+            if gameline == "wod":
+                redi = f"items:create:{item_type}"
+            elif gameline == "wta":
+                redi = f"items:werewolf:create:{item_type}"
+            elif gameline == "mta":
+                redi = f"items:mage:create:{item_type}"
+            return redirect(redi)
+        elif action == "random":
+            if request.user.is_authenticated:
+                user = request.user
+            else:
+                user = None
+            item = self.items[request.POST["item_type"]].objects.create(
+                name=request.POST["name"], owner=user
+            )
+            if request.POST["rank"] is None:
+                rank = None
+            else:
+                rank = int(request.POST["rank"])
+            try:
+                item.random(rank=rank)
+            except:
+                raise Http404
+            item.save()
+            return redirect(item.get_absolute_url())
         return render(request, "items/index.html", context)
 
     def get_context(self, kwargs):
@@ -73,11 +135,5 @@ class ItemIndexView(View):
             chron_dict[chron] = c
 
         context["chron_dict"] = chron_dict
-
-        return context
-
-        items = ItemModel.objects.filter(chronicle=None).order_by("name")
-        context = {}
-        context["items"] = items
-        # context["form"] = RandomItemForm
+        context["creation_form"] = ItemCreationForm(gameline=gameline)
         return context
