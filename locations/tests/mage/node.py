f316f8ae9b622680c420a7482fe6375955824c73
Charles Siegel <charles.m.siegel@gmail.com>
2024-08-22 03:55:40 -0700

    Fixed typo in random node tests

--- locations/tests/mage/node.py
+++ locations/tests/mage/node.py
@@ -205,7 +205,7 @@ class TestNode(TestCase):
 
 class TestRandomNode(TestCase):
     def setUp(self):
-        node = ObjectType.objects.create(name="Node", type="loc", gameline="mta")
+        node = ObjectType.objects.create(name="node", type="loc", gameline="mta")
         for i in range(1, 11):
             Resonance.objects.create(name=f"Resonance {i}")
         for i in range(1, 6):
