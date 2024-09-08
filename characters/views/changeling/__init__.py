6db9e024667acaf379670075f69a459991690921
Charles Siegel <charles.m.siegel@gmail.com>
2024-09-08 07:00:07 -0700

    adding views to init

--- characters/views/changeling/__init__.py
+++ characters/views/changeling/__init__.py
@@ -1,6 +1,6 @@
 from .changeling import ChangelingCreateView, ChangelingDetailView, ChangelingUpdateView
 from .ctdhuman import CtDHumanCreateView, CtDHumanDetailView, CtDHumanUpdateView
-from .house import HouseCreateView, HouseDetailView, HouseUpdateView
-from .kith import KithCreateView, KithDetailView, KithUpdateView
-from .legacy import LegacyCreateView, LegacyDetailView, LegacyUpdateView
+from .house import HouseCreateView, HouseDetailView, HouseUpdateView, HouseListView
+from .kith import KithCreateView, KithDetailView, KithUpdateView, KithListView
+from .legacy import LegacyCreateView, LegacyDetailView, LegacyUpdateView, LegacyListView
 from .motley import MotleyCreateView, MotleyDetailView, MotleyUpdateView
