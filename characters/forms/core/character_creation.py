25b54494307bb20c426390b00b8e8104201a4234
Charles Siegel <charles.m.siegel@gmail.com>
2024-09-13 07:44:27 -0700

    some css styling with bootstrap

--- characters/forms/core/character_creation.py
+++ characters/forms/core/character_creation.py
@@ -3,9 +3,20 @@ from game.models import ObjectType
 
 
 class CharacterCreationForm(forms.Form):
-    char_type = forms.ChoiceField(choices=[])
-    name = forms.CharField(max_length=100, label="Name", required=False)
-    rank = forms.IntegerField(initial=1, max_value=5)
+    char_type = forms.ChoiceField(
+        choices=[], widget=forms.Select(attrs={"class": "btn btn-primary dropdown"})
+    )
+    name = forms.CharField(
+        max_length=100,
+        label="Name",
+        required=False,
+        widget=forms.TextInput(attrs={"class": "btn btn-primary"}),
+    )
+    rank = forms.IntegerField(
+        initial=1,
+        max_value=5,
+        widget=forms.NumberInput(attrs={"class": "btn btn-primary"}),
+    )
 
     def __init__(self, *args, **kwargs):
         gameline = kwargs.pop("gameline")
@@ -14,3 +25,6 @@ class CharacterCreationForm(forms.Form):
             (x.name, x.name.replace("_", " ").title())
             for x in ObjectType.objects.filter(type="char", gameline=gameline)
         ]
+
+
+#         self.fields['char_type'].widget = forms.Select(attrs={"class": "btn btn-primary dropdown"})
