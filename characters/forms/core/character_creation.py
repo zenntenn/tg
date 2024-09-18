64ce82935907115956416da7c8566d9d3bb34fb2
Charles Siegel <charles.m.siegel@gmail.com>
2024-09-18 05:42:07 -0700

    Character Index: Change form Fixes #326

--- characters/forms/core/character_creation.py
+++ characters/forms/core/character_creation.py
@@ -4,20 +4,11 @@ from game.models import ObjectType
 
 class CharacterCreationForm(forms.Form):
     char_type = forms.ChoiceField(choices=[])
-    name = forms.CharField(
-        max_length=100,
-        label="Name",
-        required=False,
-    )
-    rank = forms.IntegerField(
-        initial=1,
-        max_value=5,
-    )
 
     def __init__(self, *args, **kwargs):
         gameline = kwargs.pop("gameline")
         super().__init__(*args, **kwargs)
         self.fields["char_type"].choices = [
             (x.name, x.name.replace("_", " ").title())
-            for x in ObjectType.objects.filter(type="char", gameline=gameline)
+            for x in ObjectType.objects.filter(type="char", gameline=gameline) if x.name in ["mage"]
         ]
