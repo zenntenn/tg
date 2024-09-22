dc92f92521e68f750a8a334168504fcf470b5ad7
Charles Siegel <charles.m.siegel@gmail.com>
2024-09-22 16:24:10 -0700

    Implement circular paradox/quintessence tracker Fixes #375

--- characters/models/mage/mage.py
+++ characters/models/mage/mage.py
@@ -176,6 +176,13 @@ class Mage(MtAHuman):
 
     def add_ability(self, ability, maximum=5):
         return add_dot(self, ability, maximum)
+    
+    @staticmethod
+    def get_paradox_wheel():
+        return list(range(20))
+    
+    def get_inverted_paradox(self):
+        return 19 - self.paradox
 
     def get_spheres(self):
         return {
