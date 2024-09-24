f77e8b4b8939d513f129062e2e16893ab6c5ff94
Charles Siegel <charles.m.siegel@gmail.com>
2024-09-24 08:29:49 -0700

    Converted Human and MtAHuman backgrounds to BackgroundRating objects and built properties to keep most things stable

--- characters/models/core/background.py
+++ characters/models/core/background.py
@@ -1,5 +1,20 @@
 from characters.models.core.statistic import Statistic
+from django.db import models
 
 
 class Background(Statistic):
     type = "background"
+
+    models.IntegerField(default=1)
+
+
+class BackgroundRating(models.Model):
+    bg = models.ForeignKey(Background, on_delete=models.SET_NULL, null=True)
+    char = models.ForeignKey(
+        "characters.Human",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="backgrounds",
+    )
+    rating = models.IntegerField(default=0)
+    note = models.CharField(default="", max_length=100)
