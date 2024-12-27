7720cfa07263b0b743f49b33fa2914f505dee8d2
Charles Siegel <charles.m.siegel@gmail.com>
2024-12-27 13:23:53 -0800

    hiding generic spirits

--- populate_db/spirits.py
+++ populate_db/spirits.py
@@ -1,23 +1,28 @@
 from characters.models.werewolf.charm import SpiritCharm
 from characters.models.werewolf.spirit_character import SpiritCharacter
 
-SpiritCharacter.objects.get_or_create(
+x = SpiritCharacter.objects.get_or_create(
     name="Deer", willpower=4, rage=4, gnosis=6, essence=14, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 368)
+x.display = False
+x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Falcon", willpower=8, rage=6, gnosis=5, essence=19, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 368)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Swift Flight"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Snake", willpower=5, rage=6, gnosis=8, essence=19, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 368)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Paralyzing Stare"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Wolf", willpower=6, rage=7, gnosis=5, essence=18, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 368)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Tracking"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Glade Child (Sapling)",
@@ -28,6 +33,7 @@ x = SpiritCharacter.objects.get_or_create(
     display=False,
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 368)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Cleanse the Blight", "Realm Sense"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Glade Child (Mature)",
@@ -38,6 +44,7 @@ x = SpiritCharacter.objects.get_or_create(
     display=False,
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 368)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Cleanse the Blight", "Realm Sense"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Glade Child (Ancient)",
@@ -48,16 +55,19 @@ x = SpiritCharacter.objects.get_or_create(
     display=False,
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 368)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Cleanse the Blight", "Realm Sense"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Lune", willpower=8, rage=4, gnosis=7, essence=19, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 369)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Open Moon Bridge"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Stormcrows", willpower=9, rage=7, gnosis=6, essence=22, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 369)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Create Wind", "Tracking"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="The Wendigo", willpower=7, rage=10, gnosis=5, essence=32, display=False
@@ -67,6 +77,7 @@ x.charms.set(
         name__in=["Blast", "Create Wind", "Freeze", "Materialize", "Tracking"]
     )
 )
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="The Wild Hunt (Huntsman)",
@@ -77,6 +88,7 @@ x = SpiritCharacter.objects.get_or_create(
     display=False,
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 369)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Armor", "Materialize", "Tracking"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="The Wild Hunt (The Hounds)",
@@ -87,11 +99,13 @@ x = SpiritCharacter.objects.get_or_create(
     display=False,
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 369)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Materialize", "Tracking"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Ancestor Spirit", willpower=6, rage=8, gnosis=7, essence=21, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 370)
 x.charms.set(SpiritCharm.objects.filter(name__in=[]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Earth Elemental", willpower=9, rage=4, gnosis=5, essence=20, display=False
@@ -99,16 +113,19 @@ x = SpiritCharacter.objects.get_or_create(
 x.charms.set(
     SpiritCharm.objects.filter(name__in=["Armor", "Materialize", "Umbraquake"])
 )
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Air Elemental", willpower=3, rage=8, gnosis=7, essence=18, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 370)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Create Wind", "Updraft"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Fire Elemental", willpower=5, rage=10, gnosis=5, essence=20, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 370)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Blast", "Create Fires"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Water Elemental", willpower=6, rage=4, gnosis=10, essence=20, display=False
@@ -116,6 +133,7 @@ x = SpiritCharacter.objects.get_or_create(
 x.charms.set(
     SpiritCharm.objects.filter(name__in=["Cleanse the Blight", "Flood", "Healing"])
 )
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Glass Elemental", willpower=4, rage=7, gnosis=7, essence=18, display=False
@@ -123,6 +141,7 @@ x = SpiritCharacter.objects.get_or_create(
 x.charms.set(
     SpiritCharm.objects.filter(name__in=["Blast", "Materialize", "Shatter Glass"])
 )
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Electricity Elemental",
@@ -137,21 +156,25 @@ x.charms.set(
         name__in=["Blast", "Control Electrical Systems", "Short Out"]
     )
 )
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Chimerling", willpower=3, rage=5, gnosis=10, essence=18, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 371)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Shapeshift"]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Engling", willpower=5, rage=1, gnosis=10, essence=16, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 371)
 x.charms.set(SpiritCharm.objects.filter(name__in=[]))
+x.display = False
 x.save()
 x = SpiritCharacter.objects.get_or_create(
     name="Curiosi", willpower=5, rage=3, gnosis=9, essence=17, display=False
 )[0].add_source("Werewolf: the Apocalypse 20th Anniversary Edition", 371)
 x.charms.set(SpiritCharm.objects.filter(name__in=["Illuminate"]))
+x.display = False
 x.save()
 
 for spirit in SpiritCharacter.objects.all():
