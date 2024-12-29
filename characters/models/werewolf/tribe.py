f34eb0b85e1eab4c2212335c77f1872f678ec58e
Charles Siegel <charles.m.siegel@gmail.com>
2024-12-29 12:27:21 -0800

    Tribe displays gifts

--- characters/models/werewolf/tribe.py
+++ characters/models/werewolf/tribe.py
@@ -1,3 +1,4 @@
+from characters.models.werewolf.gift import Gift, GiftPermission
 from core.models import Model
 from django.db import models
 from django.urls import reverse
@@ -24,3 +25,38 @@ class Tribe(Model):
 
     def get_heading(self):
         return "wta_heading"
+
+    def get_camps(self):
+        from characters.models.werewolf.camp import Camp
+
+        return Camp.objects.filter(tribe=self)
+
+    def get_gifts_by_rank(self, rank):
+        tribe_permission = GiftPermission.objects.get(
+            shifter="werewolf", condition=self.name
+        )
+        return Gift.objects.filter(rank=rank, allowed=tribe_permission)
+
+    @property
+    def gifts_1(self):
+        return self.get_gifts_by_rank(1)
+
+    @property
+    def gifts_2(self):
+        return self.get_gifts_by_rank(2)
+
+    @property
+    def gifts_3(self):
+        return self.get_gifts_by_rank(3)
+
+    @property
+    def gifts_4(self):
+        return self.get_gifts_by_rank(4)
+
+    @property
+    def gifts_5(self):
+        return self.get_gifts_by_rank(5)
+
+    @property
+    def gifts_6(self):
+        return self.get_gifts_by_rank(6)
